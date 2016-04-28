e67c25d258e66b01d5ed98dc3a462effce98814b
nur faten jamaludin <aten93.hzyra@gmail.com>
2016-04-28 11:07:49 +0800

    no message

--- include/config.php
+++ include/config.php
@@ -3,11 +3,11 @@
  * Database configuration
  */
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'gcm_chat_new');
+define('DB_NAME', 'gcm_chat');
 
-define("GOOGLE_API_KEY", "AIzaSyDKk_ew7Vi4FZnMzu6GCY5nLb4xZG8muvI");
+define("GOOGLE_API_KEY", "AIzaSyDqtZwMESoKS-1qGab8F--UKkI6eAEgOXM");
 
 // push notification flags
 define('PUSH_FLAG_CHATROOM', 1);
